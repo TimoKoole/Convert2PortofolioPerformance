d65ab64ef1d0b5e4aa478dad0fe7d0241da58356
Timo <timo.koole@gmail.com>
2019-12-10 11:36:58 +0100

    Also include taxes for dividend

--- degiro/deGiroConverterAccount.py
+++ degiro/deGiroConverterAccount.py
@@ -8,13 +8,14 @@ import datetime
 from currency_converter import CurrencyConverter
 
 c = CurrencyConverter(fallback_on_missing_rate=True, fallback_on_wrong_date=True)
-convert_isin=['IE00B3RBWM25', 'IE00B0M62Q58', 'IE0031442068', 'IE00BZ163M45']
+convert_isin = ['IE00B3RBWM25', 'IE00B0M62Q58', 'IE0031442068', 'IE00BZ163M45']
+
 
 def convert_currency(isin, value, date):
     if isin in convert_isin:
         try:
-            converted = c.convert(float(value.replace(',','.')) , 'USD', 'EUR', date=date)
-            return ('%.2f' % converted).replace('.',',')
+            converted = c.convert(float(value.replace(',', '.')), 'USD', 'EUR', date=date)
+            return ('%.2f' % converted).replace('.', ',')
         except Exception as e:
             logging.exception("message")
             print(isin + str(value) + str(date))
@@ -24,7 +25,7 @@ def convert_currency(isin, value, date):
 
 class DeGiroConverterAccount:
     EXPORT_COLUMNS = ["Date", "ISIN", "Value",
-                      "shares", "Fees", 'Transaction Currency']
+                      "shares", "Fees", 'Transaction Currency', 'Type']
 
     def dateparse(self, date):
         try:
@@ -32,15 +33,13 @@ class DeGiroConverterAccount:
         except:
             return ''
 
-
     def __init__(self, inputfile):
         self.note = 'Timo import at: ' + str(datetime.datetime.now())
         self.inputdata = pd.read_csv(inputfile, parse_dates=[
-                                     0], date_parser=self.dateparse)
+            0], date_parser=self.dateparse)
         self.df = self.filter_input()
         self.outputdata = pd.DataFrame(
             index=self.df.index, columns=self.EXPORT_COLUMNS, data=None)
-
 
     def convert(self):
         self.outputdata['Date'] = self.df['Datum'].apply(degiro.util.convert_date)
@@ -48,11 +47,13 @@ class DeGiroConverterAccount:
         self.df['Value'] = self.df.iloc[:, 8]
         self.outputdata['Transaction Currency'] = self.df['Mutatie']
         self.outputdata['Note'] = self.note
+        self.outputdata['Type'][self.df['Omschrijving'].str.contains("Dividendbelasting")] = 'Taxes'
 
         # custom overwrite
-        self.outputdata['Transaction Currency'] [self.outputdata['ISIN'].isin(convert_isin)]  = 'EUR'
+        self.outputdata['Transaction Currency'][self.outputdata['ISIN'].isin(convert_isin)] = 'EUR'
         # gapminder[gapminder['year'] == 2002]
-        self.outputdata['Value'] = self.df.apply(lambda row: convert_currency(row['ISIN'], row['Value'], row['Datum']), axis=1)
+        self.outputdata['Value'] = self.df.apply(lambda row: convert_currency(row['ISIN'], row['Value'], row['Datum']),
+                                                 axis=1)
 
     def write_outputfile(self, outputfolder, outputfile):
         os.makedirs(outputfolder, exist_ok=True)
@@ -61,9 +62,11 @@ class DeGiroConverterAccount:
 
     def filter_input(self):
         df = self.inputdata
-        df = df[(df['Omschrijving'] == "Dividend") | (
-            df['Omschrijving'] == "iDEAL storting")]
         df = df[~df["Omschrijving"].str.contains('geldmarktfonds')]
+
+        df = df[(df['Omschrijving'].str.contains("Dividend")) | (
+                df['Omschrijving'] == "iDEAL storting")]
+
         return df
 
 
